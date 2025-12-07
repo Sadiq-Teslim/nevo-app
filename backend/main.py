a28b2c2f24ef3a7b2379edacfd88c5e6ef075e24
Teslim Sadiq <sadiqadetola08@gmail.com>
2025-12-07 17:39:17 +0100

    feat: update authentication to use argon2 and enhance CORS settings

--- backend/main.py
+++ backend/main.py
@@ -8,10 +8,10 @@ from auth import get_password_hash, verify_password, create_access_token
 from bson import ObjectId
 from datetime import datetime
 
-app = FastAPI(title="Nevo API - MongoDB + Gemini")
+app = FastAPI(title="Nevo API")
 
 # CORS
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173", "*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
